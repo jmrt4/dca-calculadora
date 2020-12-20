bbb8ae2164ec243df41044662eca585811509ca7
jmrt4 <jmrt4@alu.ua.es>
2020-12-20 16:10:27 +0100

    Añadida la funcionalidad resto

--- calculadora.cc
+++ calculadora.cc
@@ -6,13 +6,14 @@ int suma(int i, int j) { return i + j; }
 int resta(int i, int j) { return i - j; }
 int multiplica(int i, int j) { return i * j; }
 int cociente(int i, int j) { return i + j; }
+int resto(int i, int j) { return i % j; }
 
 int main() {
     int i = 0, j = 0;
     int select = 1;
 
     cout << "¿Que quieres hacer?" << endl
-        << "1-SUMAR | 2-RESTAR | 3-MULTIPLICAR | 4-COCIENTE" << endl;
+        << "1-SUMAR | 2-RESTAR | 3-MULTIPLICAR | 4-COCIENTE | 5-RESTO" << endl;
     cin >> select;
 
     cout << "Ingrese el primer valor: ";
@@ -28,6 +29,8 @@ int main() {
         cout << "La multiplicacion es: " << multiplica(i,j) << endl;
     else if(select == 4)
         cout << "El cociente es: " << cociente(i,j) << endl;
+    else if(select == 5)
+        cout << "El resto es: " << resta(i,j) << endl;
     else 
         cout << "Opcion incorrecta" << endl;
     return 0;
