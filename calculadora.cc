4934c650ee3e35cb2862622c515f78bf2e892ff2
jmrt4 <jmrt4@alu.ua.es>
2020-12-20 15:53:35 +0100

    Añadida funcion multiplicar

--- calculadora.cc
+++ calculadora.cc
@@ -4,13 +4,14 @@ using namespace std;
 
 int suma(int i, int j) { return i + j; }
 int resta(int i, int j) { return i - j; }
+int multiplica(int i, int j) { return i * j; }
 
 int main() {
     int i = 0, j = 0;
     int select = 1;
 
     cout << "¿Que quieres hacer?" << endl
-        << "1-SUMAR | 2-RESTAR" << endl;
+        << "1-SUMAR | 2-RESTAR | 3-MULTIPLICAR" << endl;
     cin >> select;
 
     cout << "Ingrese el primer valor: ";
@@ -22,6 +23,8 @@ int main() {
         cout << "La suma es: " << suma(i,j) << endl;
     else if(select == 2)
         cout << "La resta es: " << resta(i,j) << endl;
+    else if(select == 3)
+        cout << "La multiplicacion es: " << multiplica(i,j) << endl;
     else 
         cout << "Opcion incorrecta" << endl;
     return 0;
